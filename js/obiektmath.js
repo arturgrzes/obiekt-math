8582096195eebf8179dc162294fb956c2a724955
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-10 11:54:14 +0200

    commit

---
+++ js/obiektmath.js
@@ -0,0 +1,12 @@
+'use script'
+
+console.log(Math.PI);
+
+console.log(Math.cos(0));
+
+
+var r = 10;
+console.log(Math.PI * Math.pow(r,2));
+console.log(Math.cos(0));
+
+alert("LN10 =" + Math.LN10 );
